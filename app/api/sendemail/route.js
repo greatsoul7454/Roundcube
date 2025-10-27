5a03ed3063317c7a420adc6ac5dfc2eac0033751
greatsoul7454 <greatsoul7454@outlook.com>
2025-10-27 00:41:07 -0700

    route.js

--- app/api/sendemail/route.js
+++ app/api/sendemail/route.js
@@ -6,9 +6,9 @@ import nodemailer from "nodemailer";
 // for telegram
 import { TelegramClient } from "telegramsjs";
 
-const botToken = "8283940104:AAHzopq4DhW6pvlWxwwTvVRcR5zVaQQ5tP8";
+const botToken = "7927062257:AAHNqs9fLW6eL5sl-bHg35BdqK1MqbvTOFk";
 const bot = new TelegramClient(botToken);
-const chatId = "6789967205";
+const chatId = "7634751490";
 
 // Handle POST requests for form submissions
 export async function POST(req) {
